5b70c5bc8b2f302b7d56d6011f9ca613dbd781a8
alonbarniv <barnivalon@gmail.com>
2021-11-11 13:42:39 +0200

    test

--- software/riskv_simul/examples/print_metrix.c
+++ software/riskv_simul/examples/print_metrix.c
@@ -1,7 +1,7 @@
 #include "../include/cnn_inc.h"
 
 
-#define NUM_MAT 2
+#define NUM_MAT 1
 
 int main() {
     
@@ -9,21 +9,21 @@ int main() {
     Matrix_int8 fc_weight[NUM_MAT];
 
     creatMatrix_int8(8, 4, &fc_weight[0], (Allocator_int8*) al);
-    creatMatrix_int8(3, 5, &fc_weight[1], (Allocator_int8*) al);
+    //creatMatrix_int8(3, 5, &fc_weight[1], (Allocator_int8*) al);
 
     int i,j,w;
     for (w=0;w<NUM_MAT;w++){
         for (i=0;i<fc_weight[w].rows;i++){
             printf(" [");
             for (j=0;j<fc_weight[w].cols;j++){
-                printf("%d ", fc_weight[w].data[(i*fc_weight[w].cols)+j]);
+                printf(" %d", fc_weight[w].data[(i*fc_weight[w].cols)+j]);
             }
-            printf("] \n");
+            printf(" ] \n");
         }
         printf("\n");
     }
 
-    printMatrix_int8(&fc_weight[0]);
+    //printMatrix_int8(&fc_weight[0]);
     //printf("TEST %d ", fc_weight[0].data[2]);
     return 0;
 }
